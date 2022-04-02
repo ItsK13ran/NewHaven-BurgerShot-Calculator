539e7e6a2076dbd202df6432779c21cb165a9d89
HM <xkiax987@hotmail.com>
2022-04-02 19:09:10 +0100

    Add Item Test 3

--- order-generator.js
+++ order-generator.js
@@ -87,7 +87,7 @@ const Menu = {
 		price: 69,
 		emoji: '😈',
 		items: ["Rimjob", "Rimjob", "Iced Tea"],
-		copypasta:  '~g~~h~ 🌄 Harrys Hairy Balls Combo 🌄~n~~b~1 Rimjob, 1 Rimjob, 1 Iced Tea ~w~|~g~ $150 ~r~LIMITED',
+		copypasta:  '~g~~h~ 🌄 Morning Glory Combo 🌄~n~~b~1 Oatmeal, 1 Bacon, 1 OJ ~w~|~g~ $150 ~r~LIMITED',
 	},
 	// Burger Meals:
 	"Burger Meals:": {
